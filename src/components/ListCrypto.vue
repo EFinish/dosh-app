6e4deb09ca522a94ead07b31043ebb028298926e
EFinish <EFinish@gmail.com>
2020-11-02 14:39:01 +0200

    cleanup, set foundation for creating components

---
+++ src/components/ListCrypto.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>LIST!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListCrypto",
+  props: {}
+};
+</script>
---
+++ src/views/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ListCrypto msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ListCrypto from "@/components/ListCrypto.vue";
+
+export default {
+  name: "List",
+  components: {
+    ListCrypto
+  }
+};
+</script>
