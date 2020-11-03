dce4716df49cd86c5a22e33b73f08ba7f1445b99
EFinish <EFinish@gmail.com>
2020-11-03 10:33:47 +0200

    more misc cleanup

--- src/views/List.vue
+++ src/views/List.vue
@@ -7,14 +7,13 @@
     </b-row>
     <b-row>
       <b-col>
-        <ListCrypto msg="Welcome to Your Vue.js App" />
+        <ListCrypto />
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
 import ListCrypto from "@/components/ListCrypto.vue";
 
 export default {
